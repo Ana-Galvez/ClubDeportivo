0aa2fee29cda3e0b3843e24b9dc7eed08d8f478d
Ana Galvez <anamariagalvez01@gmail.com>
2025-05-13 19:12:53 -0300

    fix: arreglo procedurenuevocliente

--- ClubDeportivo/ProcedureNuevoCliente.sql
+++ ClubDeportivo/ProcedureNuevoCliente.sql
@@ -1,44 +1,43 @@
- delimiter //
- create procedure NuevoCliente(
-	in IDCli int,
-  in Nom varchar(30),
-  in Ape varchar(40),
-  in FechaNac date, 
-  in Doc int, 
-  in Gen enum('F','M','Prefiero no decir'), 
-  in Dir varchar(60), in Tel varchar(30), 
-  in FechaInsc date, 
-  in AptoF tinyint, 
-  in Soc tinyint, 
-  out rta int
+DELIMITER //
+
+CREATE PROCEDURE NuevoCliente(
+    IN p_Nombre VARCHAR(30),
+    IN p_Apellido VARCHAR(40),
+    IN p_FechaNacimiento DATE,
+    IN p_DNI INT,
+    IN p_Genero VARCHAR(20),
+    IN p_Direccion VARCHAR(60),
+    IN p_Telefono VARCHAR(30),
+    IN p_FechaInscripcion DATE,
+    IN p_AptoFisico BOOLEAN,
+    IN p_Socio BOOLEAN,
+    OUT rta INT
 )
- begin
-    declare filas int default 0;
-    declare existe int default 0;
-    
-     set filas = (select count(*) from cliente);
-     if filas = 0 then
-	set filas = 1; /* consideramos a este numero como el primer numero de postulante */
-     else
-     /* -------------------------------------------------------------------------------
-		buscamos el ultimo numero de postulante almacenado para sumarle una unidad y
-		considerarla como PRIMARY KEY de la tabla
-   ___________________________________________________________________________ */
-	set filas = (select max(IDCliente) + 1 from cliente);
-		
-		/* ---------------------------------------------------------
-			para saber si ya esta almacenado el postulante
-		------------------------------------------------------- */	
-	set existe = (select count(*) from cliente where DNI = doc and IDCliente = IDCli);
-      end if;
-	 
-      if existe = 0 then	 
-	insert into cliente 
-     	values(filas,IDCli,Nom,Ape,FechaNac,Doc,Gen,Dir,Tel,FechaInsc,AptoF,Soc);
-	set rta  = filas;
-      else
-	set rta = existe;
-      end if;		 
-end //
-		end if;		 
-end //
+BEGIN
+    DECLARE existe INT DEFAULT 0;
+    DECLARE nuevoID INT DEFAULT 0;
+
+    -- Verificamos si ya existe el cliente con ese DNI
+    SET existe = (SELECT COUNT(*) FROM cliente WHERE DNI = p_DNI);
+
+    IF existe = 0 THEN
+        INSERT INTO cliente (
+            Nombre, Apellido, FechaNacimiento, DNI, Genero,
+            Direccion, Telefono, FechaInscripcion, AptoFisico, Socio
+        ) VALUES (
+            p_Nombre, p_Apellido, p_FechaNacimiento, p_DNI, p_Genero,
+            p_Direccion, p_Telefono, p_FechaInscripcion, p_AptoFisico, p_Socio
+        );
+
+        -- Obtenemos el ID autogenerado
+        SET nuevoID = LAST_INSERT_ID();
+        SET rta = nuevoID;
+
+    ELSE
+        -- Si el DNI ya existe, devolvemos -1
+        SET rta = -1;
+    END IF;
+END;
+//
+
+DELIMITER ;

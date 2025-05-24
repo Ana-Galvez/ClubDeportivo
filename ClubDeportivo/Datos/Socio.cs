a1da4849b27562a06cdbadbf3a8c34687b048947
LorenaCoheneBaez <loreley.cb15@gmail.com>
2025-05-24 19:47:14 -0300

    Agrego clase socio y cuota

---
+++ ClubDeportivo/Datos/Socio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDeportivo.Datos
+{
+    internal class Socio
+    {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreCompleto => $"{Nombre} {Apellido}";
+        public bool EsSocio { get; set; }
+        public DateTime? FechaAltaSocio { get; set; }
+    }
+}

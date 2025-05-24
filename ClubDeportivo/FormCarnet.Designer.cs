5356d4242c8fe4660755baba86989c89b68cf678
Ana Galvez <anamariagalvez01@gmail.com>
2025-05-24 00:59:53 -0300

    feat: agregado Imprimir carnet como pdf

--- ClubDeportivo/FormCarnet.Designer.cs
+++ ClubDeportivo/FormCarnet.Designer.cs
@@ -56,6 +56,8 @@
             buttonCarnetVolver = new Button();
             labelID = new Label();
             labelIDText = new Label();
+            buttonImprimirCarnet = new Button();
+            printDocument1 = new System.Drawing.Printing.PrintDocument();
             panelCarnetHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBoxCarnet).BeginInit();
             SuspendLayout();
@@ -66,7 +68,7 @@
             panelCarnetHeader.Controls.Add(labelCarnetHeader);
             panelCarnetHeader.Location = new Point(0, 0);
             panelCarnetHeader.Name = "panelCarnetHeader";
-            panelCarnetHeader.Size = new Size(801, 48);
+            panelCarnetHeader.Size = new Size(801, 42);
             panelCarnetHeader.TabIndex = 37;
             // 
             // labelCarnetHeader
@@ -74,7 +76,7 @@
             labelCarnetHeader.AutoSize = true;
             labelCarnetHeader.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             labelCarnetHeader.ForeColor = Color.White;
-            labelCarnetHeader.Location = new Point(0, 11);
+            labelCarnetHeader.Location = new Point(0, 10);
             labelCarnetHeader.Name = "labelCarnetHeader";
             labelCarnetHeader.Padding = new Padding(10, 0, 0, 0);
             labelCarnetHeader.Size = new Size(181, 25);
@@ -87,7 +89,7 @@
             labelCarnetTitulo.AutoSize = true;
             labelCarnetTitulo.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             labelCarnetTitulo.ForeColor = Color.Black;
-            labelCarnetTitulo.Location = new Point(482, 71);
+            labelCarnetTitulo.Location = new Point(482, 63);
             labelCarnetTitulo.Name = "labelCarnetTitulo";
             labelCarnetTitulo.Size = new Size(68, 25);
             labelCarnetTitulo.TabIndex = 38;
@@ -96,189 +98,189 @@
             // pictureBoxCarnet
             // 
             pictureBoxCarnet.Image = (Image)resources.GetObject("pictureBoxCarnet.Image");
-            pictureBoxCarnet.Location = new Point(28, 117);
+            pictureBoxCarnet.Location = new Point(28, 103);
             pictureBoxCarnet.Name = "pictureBoxCarnet";
-            pictureBoxCarnet.Size = new Size(258, 224);
+            pictureBoxCarnet.Size = new Size(258, 198);
             pictureBoxCarnet.TabIndex = 61;
             pictureBoxCarnet.TabStop = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(325, 141);
+            label1.Location = new Point(325, 124);
             label1.Name = "label1";
-            label1.Size = new Size(57, 17);
+            label1.Size = new Size(51, 15);
             label1.TabIndex = 40;
             label1.Text = "Nombre";
             // 
             // labelNombre
             // 
             labelNombre.AutoSize = true;
-            labelNombre.Location = new Point(417, 141);
+            labelNombre.Location = new Point(417, 124);
             labelNombre.Name = "labelNombre";
-            labelNombre.Size = new Size(43, 17);
+            labelNombre.Size = new Size(38, 15);
             labelNombre.TabIndex = 41;
             labelNombre.Text = "label2";
             // 
             // labelApellido
             // 
             labelApellido.AutoSize = true;
-            labelApellido.Location = new Point(417, 182);
+            labelApellido.Location = new Point(417, 161);
             labelApellido.Name = "labelApellido";
-            labelApellido.Size = new Size(43, 17);
+            labelApellido.Size = new Size(38, 15);
             labelApellido.TabIndex = 43;
             labelApellido.Text = "label3";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(325, 182);
+            label4.Location = new Point(325, 161);
             label4.Name = "label4";
-            label4.Size = new Size(56, 17);
+            label4.Size = new Size(51, 15);
             label4.TabIndex = 42;
             label4.Text = "Apellido";
             // 
             // labelFechaNacimiento
             // 
             labelFechaNacimiento.AutoSize = true;
-            labelFechaNacimiento.Location = new Point(417, 219);
+            labelFechaNacimiento.Location = new Point(417, 193);
             labelFechaNacimiento.Name = "labelFechaNacimiento";
-            labelFechaNacimiento.Size = new Size(43, 17);
+            labelFechaNacimiento.Size = new Size(38, 15);
             labelFechaNacimiento.TabIndex = 45;
             labelFechaNacimiento.Text = "label5";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(325, 219);
+            label6.Location = new Point(325, 193);
             label6.Name = "label6";
-            label6.Size = new Size(87, 17);
+            label6.Size = new Size(79, 15);
             label6.TabIndex = 44;
             label6.Text = "Fecha de nac.";
             // 
             // labelDNI
             // 
             labelDNI.AutoSize = true;
-            labelDNI.Location = new Point(417, 258);
+            labelDNI.Location = new Point(417, 228);
             labelDNI.Name = "labelDNI";
-            labelDNI.Size = new Size(43, 17);
+            labelDNI.Size = new Size(38, 15);
             labelDNI.TabIndex = 47;
             labelDNI.Text = "label7";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(325, 258);
+            label8.Location = new Point(325, 228);
             label8.Name = "label8";
-            label8.Size = new Size(30, 17);
+            label8.Size = new Size(27, 15);
             label8.TabIndex = 46;
             label8.Text = "DNI";
             // 
             // labelDireccion
             // 
             labelDireccion.AutoSize = true;
-            labelDireccion.Location = new Point(416, 295);
+            labelDireccion.Location = new Point(416, 260);
             labelDireccion.Name = "labelDireccion";
-            labelDireccion.Size = new Size(43, 17);
+            labelDireccion.Size = new Size(38, 15);
             labelDireccion.TabIndex = 49;
             labelDireccion.Text = "label9";
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(324, 295);
+            label10.Location = new Point(324, 260);
             label10.Name = "label10";
-            label10.Size = new Size(62, 17);
+            label10.Size = new Size(57, 15);
             label10.TabIndex = 48;
             label10.Text = "Dirección";
             // 
             // labelTelefono
             // 
             labelTelefono.AutoSize = true;
-            labelTelefono.Location = new Point(673, 141);
+            labelTelefono.Location = new Point(673, 124);
             labelTelefono.Name = "labelTelefono";
-            labelTelefono.Size = new Size(50, 17);
+            labelTelefono.Size = new Size(44, 15);
             labelTelefono.TabIndex = 51;
             labelTelefono.Text = "label11";
             // 
             // label12
             // 
             label12.AutoSize = true;
-            label12.Location = new Point(581, 141);
+            label12.Location = new Point(581, 124);
             label12.Name = "label12";
-            label12.Size = new Size(58, 17);
+            label12.Size = new Size(52, 15);
             label12.TabIndex = 50;
             label12.Text = "Teléfono";
             // 
             // labelFechaInscripcion
             // 
             labelFechaInscripcion.AutoSize = true;
-            labelFechaInscripcion.Location = new Point(673, 182);
+            labelFechaInscripcion.Location = new Point(673, 161);
             labelFechaInscripcion.Name = "labelFechaInscripcion";
-            labelFechaInscripcion.Size = new Size(50, 17);
+            labelFechaInscripcion.Size = new Size(44, 15);
             labelFechaInscripcion.TabIndex = 53;
             labelFechaInscripcion.Text = "label13";
             // 
             // label14
             // 
             label14.AutoSize = true;
-            label14.Location = new Point(581, 182);
+            label14.Location = new Point(581, 161);
             label14.Name = "label14";
-            label14.Size = new Size(89, 17);
+            label14.Size = new Size(81, 15);
             label14.TabIndex = 52;
             label14.Text = "Fecha de insc.";
             // 
             // labelGenero
             // 
             labelGenero.AutoSize = true;
-            labelGenero.Location = new Point(673, 219);
+            labelGenero.Location = new Point(673, 193);
             labelGenero.Name = "labelGenero";
-            labelGenero.Size = new Size(50, 17);
+            labelGenero.Size = new Size(44, 15);
             labelGenero.TabIndex = 55;
             labelGenero.Text = "label15";
             // 
             // labelGeneroText
             // 
             labelGeneroText.AutoSize = true;
-            labelGeneroText.Location = new Point(581, 219);
+            labelGeneroText.Location = new Point(581, 193);
             labelGeneroText.Name = "labelGeneroText";
-            labelGeneroText.Size = new Size(51, 17);
+            labelGeneroText.Size = new Size(45, 15);
             labelGeneroText.TabIndex = 54;
             labelGeneroText.Text = "Género";
             // 
             // labelAptoFisico
             // 
             labelAptoFisico.AutoSize = true;
-            labelAptoFisico.Location = new Point(673, 258);
+            labelAptoFisico.Location = new Point(673, 228);
             labelAptoFisico.Name = "labelAptoFisico";
-            labelAptoFisico.Size = new Size(50, 17);
+            labelAptoFisico.Size = new Size(44, 15);
             labelAptoFisico.TabIndex = 57;
             labelAptoFisico.Text = "label17";
             // 
             // label18
             // 
             label18.AutoSize = true;
-            label18.Location = new Point(581, 258);
+            label18.Location = new Point(581, 228);
             label18.Name = "label18";
-            label18.Size = new Size(72, 17);
+            label18.Size = new Size(66, 15);
             label18.TabIndex = 56;
             label18.Text = "Apto Físico";
             // 
             // labelSocio
             // 
             labelSocio.AutoSize = true;
-            labelSocio.Location = new Point(673, 295);
+            labelSocio.Location = new Point(673, 260);
             labelSocio.Name = "labelSocio";
-            labelSocio.Size = new Size(50, 17);
+            labelSocio.Size = new Size(44, 15);
             labelSocio.TabIndex = 59;
             labelSocio.Text = "label19";
             // 
             // label20
             // 
             label20.AutoSize = true;
-            label20.Location = new Point(581, 295);
+            label20.Location = new Point(581, 260);
             label20.Name = "label20";
-            label20.Size = new Size(52, 17);
+            label20.Size = new Size(46, 15);
             label20.TabIndex = 58;
             label20.Text = "¿Socio?";
             // 
@@ -288,9 +290,9 @@
             buttonCarnetVolver.FlatStyle = FlatStyle.Flat;
             buttonCarnetVolver.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             buttonCarnetVolver.ForeColor = Color.White;
-            buttonCarnetVolver.Location = new Point(439, 359);
+            buttonCarnetVolver.Location = new Point(517, 317);
             buttonCarnetVolver.Name = "buttonCarnetVolver";
-            buttonCarnetVolver.Size = new Size(200, 32);
+            buttonCarnetVolver.Size = new Size(200, 28);
             buttonCarnetVolver.TabIndex = 60;
             buttonCarnetVolver.Text = "Volver";
             buttonCarnetVolver.UseVisualStyleBackColor = false;
@@ -300,7 +302,7 @@
             // 
             labelID.AutoSize = true;
             labelID.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            labelID.Location = new Point(159, 359);
+            labelID.Location = new Point(159, 317);
             labelID.Name = "labelID";
             labelID.Size = new Size(45, 17);
             labelID.TabIndex = 63;
@@ -310,17 +312,36 @@
             // 
             labelIDText.AutoSize = true;
             labelIDText.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            labelIDText.Location = new Point(67, 359);
+            labelIDText.Location = new Point(67, 317);
             labelIDText.Name = "labelIDText";
             labelIDText.Size = new Size(20, 17);
             labelIDText.TabIndex = 62;
             labelIDText.Text = "Id";
             // 
+            // buttonImprimirCarnet
+            // 
+            buttonImprimirCarnet.BackColor = Color.FromArgb(0, 70, 160);
+            buttonImprimirCarnet.FlatStyle = FlatStyle.Flat;
+            buttonImprimirCarnet.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonImprimirCarnet.ForeColor = Color.White;
+            buttonImprimirCarnet.Location = new Point(298, 317);
+            buttonImprimirCarnet.Name = "buttonImprimirCarnet";
+            buttonImprimirCarnet.Size = new Size(200, 28);
+            buttonImprimirCarnet.TabIndex = 64;
+            buttonImprimirCarnet.Text = "Imprimir";
+            buttonImprimirCarnet.UseVisualStyleBackColor = false;
+            buttonImprimirCarnet.Click += buttonImprimirCarnet_Click;
+            // 
+            // printDocument1
+            // 
+            printDocument1.PrintPage += printDocument1_PrintPage;
+            // 
             // FormCarnet
             // 
-            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 397);
+            Controls.Add(buttonImprimirCarnet);
             Controls.Add(labelID);
             Controls.Add(labelIDText);
             Controls.Add(buttonCarnetVolver);
@@ -387,5 +408,7 @@
         private Button buttonCarnetVolver;
         private Label labelID;
         private Label labelIDText;
+        private Button buttonImprimirCarnet;
+        private System.Drawing.Printing.PrintDocument printDocument1;
     }
 }
--- ClubDeportivo/FormCarnet.cs
+++ ClubDeportivo/FormCarnet.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Printing;
+
 
 namespace ClubDeportivo
 {
@@ -40,6 +42,36 @@ namespace ClubDeportivo
             this.Hide();
         }
 
-       
+        private void buttonImprimirCarnet_Click(object sender, EventArgs e)
+        {
+            buttonCarnetVolver.Visible = false;
+            buttonImprimirCarnet.Visible = false;
+            PrintDocument pd = new PrintDocument();
+            pd.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            pd.PrinterSettings.PrinterName = "Microsoft Print to PDF";
+            pd.DefaultPageSettings.Landscape = true;
+            pd.Print();
+
+            MessageBox.Show("Impresión exitosa", "AVISO DEL SISTEMA",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+            buttonCarnetVolver.Visible = true;
+            buttonImprimirCarnet.Visible = true;
+            FormHome principal = new FormHome(usuario);
+            principal.Show();
+            this.Close();
+        }
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            int x = SystemInformation.WorkingArea.X;
+            int y = SystemInformation.WorkingArea.Y;
+            int ancho = this.Width;
+            int alto = this.Height;
+            Rectangle bounds = new Rectangle(x, y, ancho, alto);
+            Bitmap img = new Bitmap(ancho, alto);
+            this.DrawToBitmap(img, bounds);
+            Point p = new Point(175, 200);
+            e.Graphics.DrawImage(img, p);
+        }
     }
 }
